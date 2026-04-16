d0c4dfd743de0cc830ebf0738c8739dff87b3a88
K Satya Sai Nischal <coderstale@gmail.com>
2026-04-16 06:16:18 +0530

    Remove obsolete C simulator files

--- Makefile
+++
@@ -1,8 +0,0 @@
-CC = gcc
-CFLAGS = -fno-stack-protector -D_FORTIFY_SOURCE=0 -O0
-
-build:
-	$(CC) $(CFLAGS) src/vulnerable.c -o simulator
-
-clean:
-	rm -f simulator
--- src/payload_gen.py
+++
@@ -1,5 +0,0 @@
-# Generates a payload to overflow the 16-byte buffer
-# We send 16 'A's to fill the buffer, and 4 'B's to overflow into the next memory address.
-
-payload = "A" * 16 + "B" * 4 
-print(payload)
